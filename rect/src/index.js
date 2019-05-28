f188f920eb2a2bdd41863120a16669cf92961261
thesmallstar <manthan.surkar@gmail.com>
2019-05-28 07:18:35 +0530

    react first codes

---
+++ rect/src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(<h1>Hello Worlds</h1>, document.getElementById("root"));
+
+/* 
+
+
+Cannot import two jsx elements in one 
+
+
+ReactDOM.render(<h1>Hello Worlds</h1><p>Helloo</p>, document.getElementById("root"));
+
+is not allowed
+
+*/

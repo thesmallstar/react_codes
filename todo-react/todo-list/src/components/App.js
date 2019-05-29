281174cf7df073ad34f9dcc73acfa68acd37ad89
thesmallstar <manthan.surkar@gmail.com>
2019-05-30 00:47:45 +0530

    init todo app with basic files

---
+++ todo-react/todo-list/src/components/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <div>
+        <input type="checkbox" name="do" value="Bike" />
+        <span>Do this</span>
+      </div>
+      <div>
+        <input type="checkbox" name="do1" value="Bike" />
+        <span>Do this</span>
+      </div>
+      <div>
+        <input type="checkbox" name="do2" value="Bike" />
+        <span>Do this</span>
+      </div>
+      <div>
+        <input type="checkbox" name="do3" value="Bike" />
+        <span>Do this</span>
+      </div>
+    </div>
+  );
+}
+
+export default App;
